6efe3f09cb5623ce2fce22238aafd94e47309bf5
Taracha McCain <tarachamccain@yahoo.com>
2017-06-29 23:27:16 -0400

    Small number program

---
+++ var.py
@@ -0,0 +1,16 @@
+#interger assigned to variable
+var = 8
+print(var)
+
+#assign a flaot value to the variable
+var = 3.5
+print (var)
+
+#String value
+var = "This is it"
+print (var)
+
+#This is a boolean value
+var = True
+print (var)
+
