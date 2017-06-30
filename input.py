cf6304021958cc0e6925910d00c8257c19c18917
Taracha McCain <tarachamccain@yahoo.com>
2017-06-29 23:56:25 -0400

    interacting with Python

---
+++ input.py
@@ -0,0 +1,11 @@
+#initialized variable with a usuer-specified value
+user = input ('I and Python. What is your name?:')
+
+#Output a string and a variable value.
+print('Welcome', user)
+
+#initializing another variable with user-specified value.
+lang = input('Favorite programming language?:')
+
+#Output for 2nd variable, string and variable value.
+print(lang, 'Is','Fun', sep = '*', end ='!\n')
