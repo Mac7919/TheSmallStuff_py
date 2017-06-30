054dd3381381597d4d3a6d91b8f081df42e29773
Taracha McCain <tarachamccain@yahoo.com>
2017-06-30 19:59:03 -0400

    Commit One-Complete

---
+++ arithmetic.py
@@ -0,0 +1,22 @@
+# Doing some simple arithmetic
+a = 8
+b = 2
+
+#Using the + Operator
+print('Addition:\t', a, '+', b, '=', a + b)
+
+#Using the - Operator
+print ('Subtract:\t', a, '-', b, '=', a - b)
+
+#Using the * Operator
+print('Multiplication:\t', a, '*', b, '=', a * b)
+
+#Using the % Operator
+print('Modulus:\t', a, '%', b, '=', a % b)
+
+#Using the // (float) and / (integer) operator
+print('Division:\t', a, '//', b, '=', a//b)
+print('Floor Division:\t', a,'/', b, '=', a/b)
+
+#Using the ** operator
+print('Exponent:\t', a, '**', b, '=', a ** b)
