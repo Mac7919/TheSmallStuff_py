ea4762fee4aa7b22f16581094c39587cc0d6a074
Taracha McCain <tarachamccain@yahoo.com>
2017-08-11 02:46:03 -0400

    new commit- 1st

---
+++ comparison.py
@@ -0,0 +1,20 @@
+nil = 0
+num = 0
+max = 1
+cap = 'A'
+low = 'a'
+
+#Numeric and character equality comparisons
+print('Equality :	', nil, '==', num, nil == num)
+print('Equality :	', cap, '==', low, cap == low)
+
+#inequality comparison
+print('Inequality :	', nil, '!=', max, nil != max)
+
+#greater and lesser comparisons
+print('Greater :	', nil, '>', max, nil > max)
+print('Lesser :	', nil, '<', max, nil < max)
+
+#greater or equal and lesser or equal comparisons
+print ('More Or Equal :	', nil, '>=', num, nil >= num)
+print ('Less Or Equal :	', max, '<=', num, max <= num)
