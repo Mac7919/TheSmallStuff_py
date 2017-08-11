fd20dd08660bafa11807869c07532680d889a44a
Taracha McCain <tarachamccain@yahoo.com>
2017-08-11 03:14:59 -0400

    1st commit

---
+++ logic.py
@@ -0,0 +1,19 @@
+a = True
+b = False
+
+# using logical AND evaluations
+print ('AND Logic:')
+print ('a and a =', a and a)
+print ('a and b =', a and b)
+print ('b and b =', b and b)
+
+# using logical OR evaluations
+print ('\nOR Logic:')
+print ('a or a =', a or a)
+print ('a or b =', a or b)
+print ('b or b =', b or b)
+
+# using the logical NOT evaluations
+print('\nNOT Logic')
+print('a =', a, '\tnot a =', not a)
+print('b =', b, '\tnot b =', not b)
